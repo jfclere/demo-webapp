c81beade1db1973e33f22d16a652f60a8b726594
Jean-Frederic Clere <jfclere@gmail.com>
2024-11-13 16:55:00 +0100

    Add python test for stickyness using a proxy.

---
+++ test-demo-webapp.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import requests
+import sys
+from threading import Thread
+import queue
+
+class testdemowebapp:
+
+  def __init__(self):
+    self.url =  "http://localhost:8000/demo-1.0/demo"
+    self.counter = 0
+    self.JSESSIONID = None
+
+  # Return False if sticky True otherwise
+  def request_reponse(self):
+    headers = {'user-agent': 'test-demo-webapp'}
+    if self.JSESSIONID != None:
+      cookies = {'JSESSIONID': self.JSESSIONID}
+      r = requests.get(self.url, headers=headers, cookies=cookies)
+    else:
+      r = requests.get(self.url, headers=headers)
+    for c in r.cookies:
+      if c.name == "JSESSIONID":
+        if self.JSESSIONID == None:
+          self.JSESSIONID = c.value
+        else:
+          if self.JSESSIONID != c.value:
+            print(c.value)
+            print(self.JSESSIONID)
+            return False
+    return True
+
+  # Return False if sticky True otherwise
+  def request_reponse_loop(self, n):
+    i = 0
+    while self.request_reponse():
+      i = i + 1
+      if i == n:
+        return True
+      else:
+        continue
+    print("NOT OK!!!")  
+    return False
+
+  def start(result_queue, n, tid):
+    mydemo = testdemowebapp()
+    if mydemo.request_reponse_loop(n):
+      result_queue.put((tid, 'done'))
+    else:
+      result_queue.put((tid, 'failed'))
+    
+    
+
+if __name__ == "__main__":
+  args = sys.argv[1:]
+  count = 1
+  loop = 1000
+  if len(args) == 1:
+    count = args[0]
+  if len(args) == 2:
+    count = args[0]
+    loop = int(args[1])
+  print(count)
+
+  q = queue.Queue()
+  tarr = [] 
+  for i in range(int(count)):
+    t = Thread(target=testdemowebapp.start, args=(q, loop, i))
+    tarr.append(t)
+  for i in range(int(count)):
+    tarr[i].start()
+
+  for i in range(int(count)):
+    r = q.get()
+    if r[1] != 'done':
+      print(r)
+      print("Failed")
+      break
